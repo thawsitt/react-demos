5c617aec6ef59fe38a505f5045181c31404ee44a
Thawsitt Naing <thawsitt@cs.stanford.edu>
2018-07-26 15:28:59 -0400

    Add working demo

--- demo000/src/App.js
+++ demo000/src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
 
-class App extends Component {
+//======================================================================
+
+class Box extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Box">
+        <h3>
+          Number of clicks:{' '}
+          <span className="num-clicks">{this.props.numClicks}</span>
+        </h3>
+        <button className="btn-plus-one" onClick={this.props.onClick}>
+          +1
+        </button>
       </div>
     );
   }
 }
+
+//======================================================================
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numClicksArray: Array(this.props.numBoxes).fill(0),
+      total: 0,
+    };
+  }
+
+  handleClick(i) {
+    const numClicksArray = this.state.numClicksArray.slice();
+    numClicksArray[i] = numClicksArray[i] + 1;
+    const total = this.state.total + 1;
+    this.setState({
+      numClicksArray: numClicksArray,
+      total: total,
+    });
+  }
+
+  render() {
+    const boxes = this.state.numClicksArray.map((value, i) => (
+      <Box key={i} onClick={() => this.handleClick(i)} numClicks={value} />
+    ));
+    return (
+      <div>
+        <h2>Total: {this.state.total}</h2>
+        {boxes}
+      </div>
+    );
+  }
+}
+
+//======================================================================
 
 export default App;
